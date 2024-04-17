6cb73a6ca127736b794474cfeeb6cfd42d7b8ba8
Abhishek Gaire <118052504+Abhishek-Gaire@users.noreply.github.com>
2024-04-17 19:28:47 +0545

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,39 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.get("/api/:date?", (req,res) => {
+  const dateString = req.params.date;
+  console.log(dateString)
+  if(!dateString){
+    let date = new Date();
+    return res.json({
+      "unix":date.valueOf(),
+      "utc":date.toUTCString(),
+    })
+  }
+  if(dateString.length > 10){
+    
+    let parsedDate = parseInt(dateString)
+    let date = new Date(parsedDate).toUTCString();
+    
+    return res.json({
+      "unix":dateString,
+      "utc":date,
+    })
+  }
+ 
+  const isDate = new Date(dateString);
+  if(isDate === "Invalid Date"){
+    return res.json({
+      "error" :isDate
+    })
+  }
+  const utcDAte = isDate.toUTCString();
+  res.json({
+    "unix" :isDate.valueOf(),
+    "utc" :utcDAte,
+  })
+})
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
